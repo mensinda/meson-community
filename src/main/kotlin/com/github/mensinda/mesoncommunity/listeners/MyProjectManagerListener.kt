02993e6ef5b8d3a97c9a1f94137056a6eda692d7
GitHub Action <action@github.com>
2021-10-30 12:12:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/mensinda/mesoncommunity/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/mensinda/mesoncommunity/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mensinda.mesoncommunity.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mensinda.mesoncommunity.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/mensinda/mesoncommunity/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mensinda/mesoncommunity/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mensinda.mesoncommunity.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mensinda.mesoncommunity.MyBundle
 
 class MyProjectService(project: Project) {
 
